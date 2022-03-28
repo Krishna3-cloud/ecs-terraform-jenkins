aae5670bd4f4b612964adcc3edf372bc394593e2
Krishna Pal <krishna.pal@tothenew.com>
2022-03-29 00:24:16 +0530

    some changes made

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   access_key = "AKIA3IX54P26KQCDFJR5"
-  secret_key = "SY47Gzm5zcMpQlD/h3mY9TCnaMGsJMmmYty+Err"
+  secret_key = "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"
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
