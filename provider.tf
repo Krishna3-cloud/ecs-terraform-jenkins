cf01d381a00e0ad995142831450959ab36388da0
Krishna Pal <krishna.pal@tothenew.com>
2022-03-29 02:44:05 +0530

    updated new changes

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  access_key = "AKIA3IX54P26HQJE2U65"
-  secret_key = "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"
+  profile = "soul"
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  default     = "ap-northeast-1"
+  default     = "eu-west-1"
   description = "aws region where our resources going to create choose"
   #replace the region as suits for your requirement
 }
